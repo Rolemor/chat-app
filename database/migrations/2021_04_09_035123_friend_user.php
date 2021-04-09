214260490c6affba062cc0b5d25af84dd5d39730
Oliver Simon <oliversimon104@gmail.com>
2021-04-09 06:07:23 +0200

    feat: add friend_user table schema to migration file

--- database/migrations/2021_04_09_035123_friend_user.php
+++ database/migrations/2021_04_09_035123_friend_user.php
@@ -13,7 +13,12 @@ class FriendUser extends Migration
      */
     public function up()
     {
-
+        Schema::create('friend_user', function (Blueprint $table) {
+            $table->bigInteger('friend_id')->unsigned()->index();
+            $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class FriendUser extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('friend_user');
     }
 }
