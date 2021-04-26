db0f90e24b249a2a691ce5e5d2e6f1da8a1e6110
Oliver Simon <oliversimon104@gmail.com>
2021-04-26 16:16:52 +0200

    feat: create friend component

---
+++ resources/js/Pages/FriendRequest/friend.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="grid grid-cols-6">
+        <div class="flex flex-col p-1 m-1 bg-blue-300 rounded-lg">
+            <div>Name: {{ friend.name }}</div>
+            <div>Email: {{ friend.email }}</div>
+            <button class="bg-white rounded-lg">Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['friend']
+}
+</script>
