6014a3d9ccb8592bd326ce97451d4f5222277128
Oliver Simon <oliversimon104@gmail.com>
2021-04-26 16:23:40 +0200

    style: add style to chat messages to separate current user and other users

--- resources/js/Pages/Chat/messageItem.vue
+++ resources/js/Pages/Chat/messageItem.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        {{ message.user.name }}: {{ message.message }}
+    <div class="text-white grid justify-items-stretch">
+        <div class="rounded-lg mt-1 p-2 bg-blue-500 max-w-3xl justify-self-end" v-if="message.user.id === user_id">
+            {{ message.user.name }}: {{ message.message }}
+        </div>
+        <div class="rounded-lg mt-1 p-2 bg-green-500 max-w-3xl justify-self-start" v-else>
+            {{ message.user.name }}: {{ message.message }}
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['message']
+    props: ['message', 'user_id'],
 }
 </script>
