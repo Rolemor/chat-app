3fcca4f08b8aba7dbf035bd6b1910636ba0a7281
Oliver Simon <oliversimon104@gmail.com>
2021-04-26 16:16:17 +0200

    feat: create friend container component

---
+++ resources/js/Pages/FriendRequest/friendsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Friends:
+    </div>
+    <div class="h-96 w-full">
+        <div class="p-2 overflow-auto">
+            <div v-for="(friend, index) in friends" :key="index">
+                <friend :friend="friend" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import friend from '@/Pages/FriendRequest/friend'
+
+export default {
+    components: { friend },
+    props: ['friends']
+}
+</script>
