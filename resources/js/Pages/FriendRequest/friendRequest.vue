a4c37d968a8a6b097fee8d48029bbc497d371268
Oliver Simon <oliversimon104@gmail.com>
2021-04-26 16:17:02 +0200

    feat: create friendRequest component

---
+++ resources/js/Pages/FriendRequest/friendRequest.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="grid grid-cols-6">
+        <div class="flex flex-col p-1 m-1 bg-green-300 rounded-lg">
+            <div>Name: {{ request.name }}</div>
+            <div>Email: {{ request.email }}</div>
+            <div class="flex-row">
+                <button v-on="" class="p-1 bg-white rounded-md">Accept</button>
+                <button class="p-1 bg-white rounded-md">Delete</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['request']
+}
+</script>
