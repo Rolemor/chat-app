b128bb0a6aba0fde2d6379c263e716a1d7f4d282
Oliver Simon <oliversimon104@gmail.com>
2021-04-26 15:21:26 +0200

    feat: set register to be the Index page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <register />
+</template>
+
+<script>
+import Register from "@/Pages/Auth/Register";
+
+export default {
+    components: {
+        Register,
+    },
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean,
+        laravelVersion: String,
+        phpVersion: String,
+    }
+}
+</script>
