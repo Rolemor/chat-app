8082e708ae7247f46afe7199f2f7eac7638f8ffe
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 04:08:24 +0200

    feat: add event handling and method for friends container

--- resources/js/Pages/FriendRequest/friendsContainer.vue
+++ resources/js/Pages/FriendRequest/friendsContainer.vue
@@ -2,10 +2,12 @@
     <div>
         Friends:
     </div>
-    <div class="h-96 w-full">
-        <div class="p-2 overflow-auto">
-            <div v-for="(friend, index) in friends" :key="index">
-                <friend :friend="friend" />
+    <div class="h-64 w-full">
+        <div class="p-2 overflow-auto flex flex-wrap">
+            <div v-for="(friend, index) in friends" :key="index" class="w-200">
+                <friend
+                    v-on:frienddeleted="friendDeleted"
+                    :friend="friend" />
             </div>
         </div>
     </div>
@@ -16,6 +18,11 @@ import friend from '@/Pages/FriendRequest/friend'
 
 export default {
     components: { friend },
-    props: ['friends']
+    props: ['friends'],
+    methods: {
+        friendDeleted() {
+            this.$emit('frienddeleted');
+        }
+    }
 }
 </script>
