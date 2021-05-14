4060a64d69f721e377eebdf0dfd6088e3cee983e
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 04:11:02 +0200

    feat: add style&button with method to delete (my)friend request

--- resources/js/Pages/FriendRequest/friendRequest.vue
+++ resources/js/Pages/FriendRequest/friendRequest.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="grid grid-cols-6">
-        <div class="flex flex-col p-1 m-1 bg-green-300 rounded-lg">
-            <div>Name: {{ request.name }}</div>
-            <div>Email: {{ request.email }}</div>
-            <div class="flex-row">
-                <button v-on="" class="p-1 bg-white rounded-md">Accept</button>
-                <button class="p-1 bg-white rounded-md">Delete</button>
-            </div>
+    <div class="flex flex-col p-1 m-1 bg-green-300 rounded-lg shadow-lg">
+        <div>Name: {{ request.name }}</div>
+        <div>Email: {{ request.email }}</div>
+        <div class="flex-row">
+            <button v-on:click="deleteFriendRequest(request.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md">Delete</button>
         </div>
     </div>
 </template>
 
 <script>
+import Button from "@/Jetstream/Button";
+
 export default {
-    props: ['request']
+    components: {Button},
+    props: ['request'],
+    methods: {
+        deleteFriendRequest(id) {
+            axios.get('/removeFriendRequest/' + id)
+                .then( response => {
+                    this.$emit('requestdeleted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        }
+    }
 }
 </script>
