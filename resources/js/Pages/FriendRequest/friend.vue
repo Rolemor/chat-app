7b05a1fc2ddb37106d457e7ea65e24cb96471d8f
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 04:12:44 +0200

    feat: add style&button with method to delete friend

--- resources/js/Pages/FriendRequest/friend.vue
+++ resources/js/Pages/FriendRequest/friend.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="grid grid-cols-6">
-        <div class="flex flex-col p-1 m-1 bg-blue-300 rounded-lg">
-            <div>Name: {{ friend.name }}</div>
-            <div>Email: {{ friend.email }}</div>
-            <button class="bg-white rounded-lg">Delete</button>
+    <div class="flex flex-col p-1 m-1 bg-blue-400 rounded-lg shadow-lg">
+        <div>Name: {{ friend.name }}</div>
+        <div>Email: {{ friend.email }}</div>
+        <div class="flex-row">
+            <button v-on:click="deleteFriend(friend.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md shadow">Delete</button>
         </div>
     </div>
 </template>
 
 <script>
+import Button from "@/Jetstream/Button";
+
 export default {
-    props: ['friend']
+    components: {Button},
+    props: ['friend'],
+    methods: {
+        deleteFriend(id) {
+            axios.get('/toggleFriend/' + id)
+                .then( response => {
+                    this.$emit('frienddeleted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        }
+    }
 }
 </script>
