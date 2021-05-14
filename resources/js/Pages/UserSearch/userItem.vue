28a979fcafedc433c84c21eef19b4e0b7ba98740
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 04:17:50 +0200

    feat: create user item component with style

---
+++ resources/js/Pages/UserSearch/userItem.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="flex flex-col p-1 m-1 bg-blue-400 rounded-lg shadow-lg" v-if="user.relationship === 1">
+        <div>{{ user.name }}</div>
+        <div>Friend</div>
+        <div class="flex-row">
+            <button v-on:click="deleteFriend(user.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md shadow">Delete</button>
+        </div>
+    </div>
+    <div class="flex flex-col p-1 m-1 bg-green-400 rounded-lg shadow-lg" v-else-if="user.relationship === 2">
+        <div>{{ user.name }}</div>
+        <div>My Request</div>
+        <div class="flex-row">
+            <button v-on:click="deleteFriendRequest(user.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md">Delete</button>
+        </div>
+    </div>
+    <div class="flex flex-col p-1 m-1 bg-yellow-400 rounded-lg shadow-lg" v-else-if="user.relationship === 3">
+        <div>{{ user.name }}</div>
+        <div>Request to me</div>
+        <div class="flex-row">
+            <button v-on:click="acceptFriendRequest(user.id)" class="p-1 mr-1 bg-white hover:bg-blue-400 hover:text-white rounded-md">Accept</button>
+            <button v-on:click="deleteFriendRequestOf(user.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md">Delete</button>
+        </div>
+    </div>
+    <div class="flex flex-col p-1 m-1 bg-gray-200 rounded-lg shadow-lg" v-else>
+        <div>{{ user.name }}</div>
+        <div>Not Friend</div>
+        <div class="flex-row">
+            <button v-on:click="addFriendRequest(user.id)" class="place-self-end p-1 bg-white hover:bg-green-500 hover:text-white rounded-md shadow">Add Friend Request</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import Button from "@/Jetstream/Button";
+
+export default {
+    components: {Button},
+    props: ['user'],
+    methods: {
+        addFriendRequest(id) {
+            axios.get('/addFriendRequest/' + id)
+                .then( response => {
+                    this.$emit('addrequest');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        },
+        deleteFriend(id) {
+            axios.get('/toggleFriend/' + id)
+                .then( response => {
+                    this.$emit('frienddeleted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        },
+        deleteFriendRequest(id) {
+            axios.get('/removeFriendRequest/' + id)
+                .then( response => {
+                    this.$emit('requestdeleted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        },
+        deleteFriendRequestOf(id) {
+            axios.get('/removeFriendRequestOf/' + id)
+                .then( response => {
+                    this.$emit('requestofdeleted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        },
+        acceptFriendRequest(id) {
+            axios.get('/acceptFriendRequest/' + id)
+                .then( response => {
+                    this.$emit('requestaccepted');
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+        }
+    }
+}
+</script>
