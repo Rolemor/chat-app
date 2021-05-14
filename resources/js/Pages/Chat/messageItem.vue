e5e08bc59088d444b06f8855a68ed14f0a386a8f
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 03:58:14 +0200

    style: separate user name and message into 2 div

--- resources/js/Pages/Chat/messageItem.vue
+++ resources/js/Pages/Chat/messageItem.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="text-white grid justify-items-stretch">
-        <div class="rounded-lg mt-1 p-2 bg-blue-500 max-w-3xl justify-self-end" v-if="message.user.id === user_id">
-            {{ message.user.name }}: {{ message.message }}
+        <div class="rounded-lg mt-1 p-2 bg-blue-500 max-w-3xl justify-self-end shadow-lg" v-if="message.user.id === user_id">
+            <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
+            <div class="inline-block">{{ message.message }}</div>
         </div>
-        <div class="rounded-lg mt-1 p-2 bg-green-500 max-w-3xl justify-self-start" v-else>
-            {{ message.user.name }}: {{ message.message }}
+        <div class="rounded-lg mt-1 p-2 bg-green-500 max-w-3xl justify-self-start shadow-lg" v-else>
+            <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
+            <div class="inline-block">{{ message.message }}</div>
         </div>
     </div>
 </template>
