bb57df1a233839e0f1457633613181e1018af3d1
Oliver Simon <oliversimon104@gmail.com>
2021-05-14 04:04:23 +0200

    feat: add event handling & methods for friend request container

--- resources/js/Pages/FriendRequest/friendRequestContainer.vue
+++ resources/js/Pages/FriendRequest/friendRequestContainer.vue
@@ -1,21 +1,51 @@
 <template>
-    <div>
+    <div class="p-2 m-3 bg-gray-200 text-xl rounded-lg shadow-lg">
         Friend Requests:
     </div>
-    <div class="h-48 w-full">
-        <div class="p-2 overflow-auto">
-            <div v-for="(request, index) in friendRequests" :key="index">
-                <friend-request :request="request" />
+    <div class="h-64 w-full overflow-auto">
+        <div class="small-screen p-2">
+            <div class="half-w">
+                <div class="p-1 m-1 bg-green-300 rounded-lg shadow-lg">My Requests:</div>
+                <div class="flex flex-wrap">
+                    <div v-for="(request, index) in friendRequests" :key="index" class="w-200">
+                        <friend-request
+                            v-on:requestdeleted="requestDeleted"
+                            :request="request" />
+                    </div>
+                </div>
+            </div>
+            <div class="half-w">
+                <div class="p-1 m-1 bg-yellow-300 rounded-lg shadow-lg">Requests for me:</div>
+                <div class="flex flex-wrap">
+                    <div v-for="(requestOf, index) in friendRequestsOf" :key="index" class="w-200">
+                        <friend-request-of
+                            v-on:requestofdeleted="requestOfDeleted"
+                            v-on:requestaccepted="requestAccepted"
+                            :requestOf="requestOf" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import friendRequest from '@/Pages/FriendRequest/friendRequest'
+import friendRequest from '@/Pages/FriendRequest/friendRequest';
+import friendRequestOf from "@/Pages/FriendRequest/friendRequestOf";
 
 export default {
-    components: { friendRequest },
-    props: ['friendRequests']
+    components: { friendRequest, friendRequestOf },
+    props: ['friendRequests', 'friendRequestsOf'],
+    methods: {
+        requestDeleted() {
+            this.$emit('requestdeleted');
+        },
+        requestOfDeleted() {
+            this.$emit('requestofdeleted');
+        },
+        requestAccepted() {
+            this.$emit('requestaccepted');
+        },
+    }
 }
 </script>
