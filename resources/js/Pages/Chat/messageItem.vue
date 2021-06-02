e033686816ac4eec17a6477db1e4041e5a3b0bb2
Oliver Simon <oliversimon104@gmail.com>
2021-06-02 14:05:11 +0200

    feat: add profile pictures to chat users

--- resources/js/Pages/Chat/messageItem.vue
+++ resources/js/Pages/Chat/messageItem.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="text-white grid justify-items-stretch">
-        <div class="rounded-lg mt-1 p-2 bg-blue-500 max-w-3xl justify-self-end shadow-lg" v-if="message.user.id === user_id">
-            <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
-            <div class="inline-block">{{ message.message }}</div>
+        <div class="justify-self-end shadow-lg" v-if="message.user.id === user_id">
+            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                <img class="h-8 w-8 rounded-full object-cover" :src="message.user.profile_photo_url" :alt="message.user.name" />
+            </div>
+            <div class="rounded-lg mt-1 p-2 bg-blue-500 max-w-3xl">
+                <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
+                <div class="inline-block">{{ message.message }}</div>
+            </div>
+
         </div>
-        <div class="rounded-lg mt-1 p-2 bg-green-500 max-w-3xl justify-self-start shadow-lg" v-else>
-            <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
-            <div class="inline-block">{{ message.message }}</div>
+        <div class=" justify-self-start shadow-lg" v-else>
+            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out inline-block">
+                <img class="h-8 w-8 rounded-full object-cover" :src="message.user.profile_photo_url" :alt="message.user.name" />
+            </div>
+            <div class="rounded-lg mt-1 p-2 bg-green-500 max-w-3xl inline-block">
+                <div class="inline-block mr-2 text-xl">{{ message.user.name }}:</div>
+                <div class="inline-block">{{ message.message }}</div>
+            </div>
         </div>
     </div>
 </template>
