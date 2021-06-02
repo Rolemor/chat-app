b06271c7be35b902f80ce6bbce9566628e4295bd
Oliver Simon <oliversimon104@gmail.com>
2021-06-02 14:07:04 +0200

    feat: display friends profile picture

--- resources/js/Pages/FriendRequest/friend.vue
+++ resources/js/Pages/FriendRequest/friend.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="flex flex-col p-1 m-1 bg-blue-400 rounded-lg shadow-lg">
-        <div>Name: {{ friend.name }}</div>
-        <div>Email: {{ friend.email }}</div>
+        <div>
+            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                <img class="h-8 w-8 rounded-full object-cover" :src="friend.profile_photo_url" :alt="friend.name" />
+            </div>
+            <div>
+                <div>Name: {{ friend.name }}</div>
+                <div>Email: {{ friend.email }}</div>
+            </div>
+        </div>
+
         <div class="flex-row">
             <button v-on:click="deleteFriend(friend.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md shadow">Delete</button>
         </div>
