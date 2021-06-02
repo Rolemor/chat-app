e0a2056fabe950f3ed21bc6fc466c63e22a8d930
Oliver Simon <oliversimon104@gmail.com>
2021-06-02 14:09:31 +0200

    feat: display friend requesters profile picture

--- resources/js/Pages/FriendRequest/friendRequestOf.vue
+++ resources/js/Pages/FriendRequest/friendRequestOf.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="flex flex-col p-1 m-1 bg-yellow-300 rounded-lg shadow-lg">
-        <div>Name: {{ requestOf.name }}</div>
-        <div>Email: {{ requestOf.email }}</div>
+        <div>
+            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                <img class="h-8 w-8 rounded-full object-cover" :src="requestOf.profile_photo_url" :alt="requestOf.name" />
+            </div>
+            <div>
+                <div>Name: {{ requestOf.name }}</div>
+                <div>Email: {{ requestOf.email }}</div>
+            </div>
+        </div>
         <div class="flex-row">
             <button v-on:click="acceptFriendRequest(requestOf.id)" class="p-1 mr-1 bg-white hover:bg-blue-400 hover:text-white rounded-md">Accept</button>
             <button v-on:click="deleteFriendRequestOf(requestOf.id)" class="place-self-end p-1 bg-white hover:bg-red-500 hover:text-white rounded-md">Delete</button>
